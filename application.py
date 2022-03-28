9b83e22304260ae70e5af5c1623b063e752dce92
Sleep <git.sleep@gmail.com>
2022-03-28 14:17:45 +0100

    Frame Update with preliminary documentation

--- application.py
+++ application.py
@@ -1,14 +1,23 @@
 import tkinter as tk
 
+# Variables for size of window:
 Height = 720
 Width = 1280
 
+# Begin "loop" of interface:
 root = tk.Tk()
 
+# Create and pack canvas (window):
 canvas = tk.Canvas(root, height = Height, width = Width)
 canvas.pack()
 
+# Create and place frame (on top of, and relative to, canvas):
+frame = tk.Frame(root, bg='blue')
+frame.place(relwidth=0.8, relheight=0.8)
+
+# Create and pack button:
 button = tk.Button(root, text="Test button", fg='white', bg='black')
 button.pack()
 
+# End "loop" of interface:
 root.mainloop()
