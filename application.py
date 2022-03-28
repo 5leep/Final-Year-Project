5e1aa3bd3a23c9e22ac61c72566d0385e4996e28
Sleep <git.sleep@gmail.com>
2022-03-28 14:10:28 +0100

    Canvas Update

--- application.py
+++ application.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 
+Height = 720
+Width = 1280
+
 root = tk.Tk()
+
+canvas = tk.Canvas(root, height = Height, width = Width)
+canvas.pack()
 
 button = tk.Button(root, text="Test button", fg='white', bg='black')
 button.pack()
