dbb82c0d8bdc5cac830562fb5e912e35f0880b31
Sleep <git.sleep@gmail.com>
2022-03-28 14:29:18 +0100

    Minor relative sizing changes + design choices

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 
 # Variables for size of window:
-Height = 720
-Width = 1280
+Height = 700
+Width = 800
 
 # Begin "loop" of interface:
 root = tk.Tk()
@@ -12,11 +12,11 @@ canvas = tk.Canvas(root, height = Height, width = Width)
 canvas.pack()
 
 # Create and place frame (on top of, and relative to, canvas):
-frame = tk.Frame(root, bg='blue')
-frame.place(relwidth=0.8, relheight=0.8)
+frame = tk.Frame(root, bg='#806ba9')
+frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
 
 # Create and pack button:
-button = tk.Button(root, text="Test button", fg='white', bg='black')
+button = tk.Button(frame, text="Test button", fg='white', bg='black')
 button.pack()
 
 # End "loop" of interface:
