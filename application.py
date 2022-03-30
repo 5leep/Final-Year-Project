ed909dc328f3dfb44e273af8b8d2c4c368beb4fa
Sleep <git.sleep@gmail.com>
2022-03-30 17:21:22 +0100

    ICO Update

--- application.py
+++ application.py
@@ -1,23 +1,43 @@
-import tkinter as tk
 
-# Variables for size of window:
-Height = 700
-Width = 800
+# Imports:
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
 # Begin "loop" of interface:
 root = tk.Tk()
+root.title('FETT - Focus Enhancement and Tracking Tool')
+root.iconbitmap('media/fet.ico')
+
+# Designate height and width of app:
+appWidth = 1600
+appHeight = 900
+
+# Get screen width and height:
+screenWidth = root.winfo_screenwidth()
+screenHeight = root.winfo_screenheight()
+
+# Create x and y coordinates for window centering:
+x = (screenWidth / 2) - (appWidth / 2)
+y = (screenHeight / 2) - (appHeight / 2)
+
+# Formulae for centering window:
+root.geometry(f'{appWidth}x{appHeight}+{int(x)}+{int(y)}')
 
 # Create and pack canvas (window):
-canvas = tk.Canvas(root, height = Height, width = Width)
+canvas = tk.Canvas()
 canvas.pack()
 
 # Create and place frame (on top of, and relative to, canvas):
-frame = tk.Frame(root, bg='#806ba9')
-frame.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
+frame = tk.Frame(root, bg='#77af87')
+frame.place(relx=0.01, rely=0.01, relwidth=0.98, relheight=0.98)
 
-# Create and pack button:
-button = tk.Button(frame, text="Test button", fg='white', bg='black')
-button.pack(side='left')
+# Create and pack START button:
+button = tk.Button(frame, text="Start", fg='white', bg='black')
+button.place(x=100, y=100)
+# Create and pack STOP button:
+button = tk.Button(frame, text="Stop", fg='white', bg='black')
+button.place(x=150, y=150)
 
 # Create and pack label:
 label = tk.Label(frame, text="This is a label", fg='black', bg='white')
