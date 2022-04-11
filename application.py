56e4a53ab4550c1534a524cc8a59acd3acf67d73
Sleep <git.sleep@gmail.com>
2022-04-11 17:07:35 +0100

    The 'It Works' Update!

--- application.py
+++ application.py
@@ -1,10 +1,16 @@
 # Imports:
+import os.path
+import subprocess
+import sys
+import time
 import tkinter as tk
 import tkinter.font as font
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 
 # Begin "loop" of interface:
+import callback
 
 root = tk.Tk()
 root.title('FETT - Focus Enhancement and Tracking Tool')
@@ -29,11 +35,7 @@ root.geometry(f'{appWidth}x{appHeight}+{int(x)}+{int(y)}')
 notebook = ttk.Notebook(root)
 notebook.pack(pady=15)
 
-# Assigning variables:
-minute = StringVar()
-minute.set("00")
-second = StringVar()
-second.set("00")
+global temp, secs
 
 # Go to Statistics tab function:
 def select():
@@ -41,54 +43,109 @@ def select():
 
 
 # Creating tabs:
-frame1 = Frame(notebook, width = screenWidth, height = screenHeight, bg = "#707c74")
-frame2 = Frame(notebook, width = screenWidth, height = screenHeight, bg = "white")
+frame1 = Frame(notebook, width=screenWidth, height=screenHeight, bg="#707c74")
+frame2 = Frame(notebook, width=screenWidth, height=screenHeight, bg="white")
 
 # Packing tabs:
-frame1.pack(fill = "both", expand = 1)
-frame2.pack(fill = "both", expand = 1)
+frame1.pack(fill="both", expand=1)
+frame2.pack(fill="both", expand=1)
 
 # Adding tabs, allowing functionality and title:
-notebook.add(frame1, text = "Timer")
-notebook.add(frame2, text = "Statistics")
+notebook.add(frame1, text="Timer")
+notebook.add(frame2, text="Statistics")
 
 # Setting up fonts:
 # buttonFont1 = font.Font(family = 'Helvetica', size = 12, weight= 'bold')
-buttonFont2 = font.Font(family = 'Helvetica', size = 10)
-timerFont1 = font.Font(family = 'Calibri', size = 40, weight='bold')
+buttonFont2 = font.Font(family='Helvetica', size=10)
+timerFont1 = font.Font(family='Calibri', size=40, weight='bold')
 
 # Setting images for buttons and labels:
-Start_btn = PhotoImage(file = 'media/Start_btn.png')
-Stop_btn = PhotoImage(file = 'media/Stop_btn.png')
-ThirtyMins_btn = PhotoImage(file = 'media/30mins_btn.png')
-OneHour_btn = PhotoImage(file = 'media/1hour_btn.png')
-Pomodoro_btn = PhotoImage(file = 'media/Pomodoro_btn.png')
-Custom_btn = PhotoImage(file = 'media/Custom_btn.png')
-Base = PhotoImage(file = 'media/base.png')
+Start_btn = PhotoImage(file='media/Start_btn.png')
+Stop_btn = PhotoImage(file='media/Stop_btn.png')
+ThirtyMins_btn = PhotoImage(file='media/30mins_btn.png')
+OneHour_btn = PhotoImage(file='media/1hour_btn.png')
+Pomodoro_btn = PhotoImage(file='media/Pomodoro_btn.png')
+Custom_btn = PhotoImage(file='media/Custom_btn.png')
+Base = PhotoImage(file='media/base.png')
 
 # Blank Labels:
-blankLabel1 = Label(frame1, text = "        ", font = timerFont1, fg = "#707c74", bg = "#707c74").grid(row = 1, column = 1)
-blankLabel2 = Label(frame1, text = "        ", font = timerFont1, fg = "#707c74", bg = "#707c74").grid(row = 2, column = 1)
+blankLabel1 = Label(frame1, text="        ", font=timerFont1, fg="#707c74", bg="#707c74").grid(row=1, column=1)
+blankLabel2 = Label(frame1, text="        ", font=timerFont1, fg="#707c74", bg="#707c74").grid(row=2, column=1)
 # blankLabel3 = Label(frame1, text = "        ", font = timerFont1, fg = "#707c74", bg = "#707c74").grid(row = 4, column = 2)
 
 # Adding Timer Base:
-TimerBase = Label(frame1, image = Base, bg = '#707c74').grid(row = 3, column = 3, columnspan = 2, rowspan = 1, padx = 10, pady = 40)
+TimerBase = Label(frame1, image=Base, bg='#707c74').grid(row=3, column=3, columnspan=2, rowspan=1, padx=10, pady=40)
 
 # Timer Label:
-timer = Label(frame1, text = ":", font = timerFont1, fg = 'White', bg = '#3b3838').grid(row = 3, column = 3, columnspan = 2, rowspan = 1, padx = 10, pady = 40)
+timer = Label(frame1, text=":", font=timerFont1, fg='White', bg='#3b3838').grid(row=3, column=3, columnspan=2,
+                                                                                rowspan=1, padx=10, pady=40)
+root.bind('<Return>', callback)
+
+# Assigning variables:
+minute = StringVar()
+minute.set("00")
+second = StringVar()
+second.set("00")
 
 # Inputs:
-minuteEntry = Entry(frame1, width = 2, font = timerFont1, textvariable = minute).grid(row = 3, column = 3, sticky = "E", padx = 15)
-secondEntry = Entry(frame1, width = 2, font = timerFont1, textvariable = second).grid(row = 3, column = 4, sticky = "W", padx = 15)
+minuteEntry = Entry(frame1, width=2, font=timerFont1, textvariable=minute).grid(row=3, column=3, sticky="E", padx=15)
+secondEntry = Entry(frame1, width=2, font=timerFont1, textvariable=second).grid(row=3, column=4, sticky="W", padx=15)
+
+# Start function
+def start():
+    global temp
+    try:
+        # the input provided by the user is
+        # stored in here :temp
+        temp = int(minute.get()) * 60 + int(second.get())
+    except:
+        print("Please input the right value")
+    while temp > -1:
+
+        # divmod(firstvalue = temp//60, secondvalue = temp%60)
+        mins, secs = divmod(temp, 60)
+
+        # Converting the input entered in mins or secs to hours,
+        # mins ,secs(input = 110 min --> 120*60 = 6600 => 1hr :
+        # 50min: 0sec)
+        if mins > 60:
+            # divmod(firstvalue = temp//60, secondvalue
+            # = temp%60)
+            hours, mins = divmod(mins, 60)
+
+        # using format () method to store the value up to
+        # two decimal places
+        minute.set("{0:2d}".format(mins))
+        second.set("{0:2d}".format(secs))
+
+        # updating the GUI window after decrementing the
+        # temp value every time
+        root.update()
+        time.sleep(1)
+        # when temp value = 0; then a messagebox pop's up
+        # with a message:"Time's up"
+        if (temp == 0):
+            messagebox.showinfo("Time Countdown", "Time's up ")
+            minute.set("00")
+            second.set("00")
+
+        # after every one sec the value of temp will be decremented
+        # by one
+        temp -= 1
+
+# Stop function:
+def stop():
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
 
 # Adding tab1 buttons:
-startButton = Button(frame1, image = Start_btn, borderwidth = 0).grid(row = 3, column = 2, padx = 10, pady = 10)
-stopButton = Button(frame1, image = Stop_btn, borderwidth = 0).grid(row = 3, column = 5, padx = 10, pady = 10)
+startButton = Button(frame1, image=Start_btn, borderwidth=0, command=start).grid(row=3, column=2, padx=10, pady=10)
+stopButton = Button(frame1, image=Stop_btn, borderwidth=0, command=stop).grid(row=3, column=5, padx=10, pady=10)
 
-ThirtyMinsButton = Button(frame1, image = ThirtyMins_btn, borderwidth = 0).grid(row = 5, column = 2, padx = 10, pady = 10)
-OneHourButton = Button(frame1, image = OneHour_btn, borderwidth = 0).grid(row = 5, column = 3, padx = 10, pady = 10)
-PomodoroButton = Button(frame1, image = Pomodoro_btn, borderwidth = 0).grid(row = 5, column = 4, padx = 10, pady = 10)
-CustomButton = Button(frame1, image = Custom_btn, borderwidth = 0).grid(row = 5, column = 5, padx = 10, pady = 10)
+ThirtyMinsButton = Button(frame1, image=ThirtyMins_btn, borderwidth=0).grid(row=5, column=2, padx=10, pady=10)
+OneHourButton = Button(frame1, image=OneHour_btn, borderwidth=0).grid(row=5, column=3, padx=10, pady=10)
+PomodoroButton = Button(frame1, image=Pomodoro_btn, borderwidth=0).grid(row=5, column=4, padx=10, pady=10)
+CustomButton = Button(frame1, image=Custom_btn, borderwidth=0).grid(row=5, column=5, padx=10, pady=10)
 
 # Adding navigate to statistics tab button:
 # navigateButton = Button(frame1, text = "To tab 2", command = select).grid(row = 1, column = 1, padx=10, pady=10)
